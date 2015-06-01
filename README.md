b52feaad7798fc3ecb8df29cb8346aa7e46ecb70
Aditya Bhasin <adbhasin@cisco.com>
2015-06-01 11:16:05 +0530

    Fixed regex. Added wsgi.py generation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # django_mod_wsgi_bootstrap
-Bootstrap for django projects to work with mod_wsgi and APache
+Bootstrap for django projects to work with mod_wsgi and Apache
+
+Edit the config.ini to specify you app and apache configurations.
+
+Usage - ./bootsy.py <app_name>
+
+
+Note: this will only generate a VirtualHost config and wsgi.py. Manuall copy is required as of now
--- bootsy.py
+++ bootsy.py
@@ -3,25 +3,38 @@ __author__ = 'adityabhasin'
 import ConfigParser
 import re
 
-pattern = re.compile("\\$\\{(\w+\\.*)\\}")
+# pattern to match placeholders
+pattern = re.compile(r"\$\{(\w+\.*\w*\.*\w*)\}")
 config = ConfigParser.SafeConfigParser()
 config.read("config.ini")
 
-output_confg = open("sample.conf", 'w')
-template_config = open("templates/django_template.conf", 'r')
 
-for line in template_config:
-    out = line
-    if '$' in out:
-        matched = pattern.search(line)
-        if matched and matched.groups()[0]:
-            value = config.get("app", matched.groups()[0])
-            out = pattern.sub(value, out)
-        out = out.replace("${", "")
-        out = out.replace("}", "")
+def build_out(app, output_confg, template_config):
+    for line in template_config:
+        out = line
+        if '$' in out:
+            matched = pattern.findall(line)
+            if matched:
+                for match in matched:
+                    value = config.get(app, match)
+                    out = out.replace(match, value)
+            out = out.replace("${", "")
+            out = out.replace("}", "")
         output_confg.write(out)
-    else:
-        output_confg.write(line)
+    template_config.close()
+    output_confg.close()
 
-output_confg.close()
-template_config.close()
+
+def bootsy(app):
+    files = [(open(app + ".conf", 'w'),
+              open("templates/django_template.conf", 'r')), (
+             open(app + "_wsgi.py", 'w'),
+             open("templates/wsgi.template", 'r'))]
+    for v in files:
+        build_out(app, v[0], v[1])
+
+
+if __name__ == "__main__":
+    import sys
+
+    bootsy(sys.argv[1])
